9952d4380a91e46b8ba588fa7f011035d3d5e98b
zachglassman <zach.glassman@gmail.com>
2018-02-10 13:13:51 -0600

    move to google package

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
-func pull_request_handler(w http.ResponseWriter, r *http.Request) {
+type PullRequestEvent struct {
+	Action       string              `json:"action,omitempty"`
+	Number       int                 `json:"number,omitempty"`
+	PullRequest  *github.PullRequest `json:"pull_request,omitempty"`
+	Repository   *github.Repository  `json:"repository,omitempty"`
+	Sender       *github.User        `json:"sender,omitempty"`
+	Installation struct {
+		ID string `json:"id,omitempty"`
+	} `json:"installation,omitempty"`
+}
+
+type BaseData struct {
+	Text string
+}
+
+func pullRequestHandler(w http.ResponseWriter, r *http.Request, client *github.Client, ctx *context.Context) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		panic(err)
@@ -33,7 +55,15 @@ func pull_request_handler(w http.ResponseWriter, r *http.Request) {
 	case "unlabeled":
 		fallthrough
 	case "opened":
-		OpenPullRequest(&p)
+		comment := &github.IssueComment{
+			Body: github.String("Hi, I am helpbot, I will be managing this PR!"),
+		}
+		tail := strings.TrimPrefix(*p.PullRequest.URL, "https://api.github.com/repos/")
+		owner := strings.Split(tail, "/")[0]
+		repo := strings.Split(tail, "/")[1]
+		number, _ := strconv.ParseInt(strings.Split(tail, "/")[2], 10, 64)
+		log.Println(owner, repo)
+		client.Issues.CreateComment(*ctx, owner, repo, int(number), comment)
 	case "edited":
 		fallthrough
 	case "closed":
@@ -46,15 +76,24 @@ func pull_request_handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func base_handler(w http.ResponseWriter, r *http.Request) {
+func baseHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/base.html"))
 	t := BaseData{Text: "Hello, this is just a webpage"}
 	tmpl.Execute(w, t)
 }
 
 func main() {
-	http.HandleFunc("/", base_handler)
-	http.HandleFunc("/pull_request", pull_request_handler)
+	var token = os.Getenv("TOKEN")
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	http.HandleFunc("/", baseHandler)
+	http.HandleFunc("/pull_request", func(w http.ResponseWriter, r *http.Request) {
+		pullRequestHandler(w, r, client, &ctx)
+	})
 	var port = os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
