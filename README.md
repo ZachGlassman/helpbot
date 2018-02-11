a1692e331677b6413c41ed9e3e3b1bb5dbab9e7c
zachglassman <zach.glassman@gmail.com>
2018-02-10 21:14:28 -0800

    add reviewers

---
+++ README.md
@@ -0,0 +1,3 @@
+# helpbot 
+
+helpbot is a tool to help manage your github repos.  It watches your repository and does the annoyinng tasks you don't want to do. 
--- main.go
+++ main.go
@@ -80,6 +80,16 @@ func commentLabel(s string, labels []*github.Label) []string {
 	return ret
 }
 
+func handleReviews(s string) []string {
+	var regExp = regexp.MustCompile(`\/review@(\w+)`)
+	res := regExp.FindAllString(s, -1)
+	var ret []string
+	for i := 0; i < len(res); i++ {
+		ret = append(ret, strings.ToLower(strings.Split(res[i], "@")[1]))
+	}
+	return ret
+}
+
 func pullCommentHandler(w http.ResponseWriter, r *http.Request, client *github.Client, ctx *context.Context) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -117,6 +127,40 @@ func pullCommentHandler(w http.ResponseWriter, r *http.Request, client *github.C
 		repLabels, _, _ := client.Issues.ListLabels(*ctx, owner, repo, nil)
 		labels := commentLabel(*p.Comment.Body, repLabels)
 		client.Issues.AddLabelsToIssue(*ctx, owner, repo, int(number), labels)
+		// now handle reviews
+		users := handleReviews(*p.Comment.Body)
+		if len(users) > 0 {
+			var gUsers []string
+			var bUsers []string
+			collabs, _, err := client.Repositories.ListCollaborators(*ctx, owner, repo, nil)
+			if err != nil {
+				log.Println("error getting collaborators")
+				log.Println(err)
+			}
+			if len(collabs) > 0 {
+				for i := 0; i < len(collabs); i++ {
+					found := false
+					ele := collabs[i]
+					name := strings.ToLower(*ele.Login)
+					for j := 0; j < len(users); j++ {
+						if name == users[j] {
+							gUsers = append(gUsers, *ele.Login)
+							found = true
+						}
+					}
+					if found != true {
+						bUsers = append(bUsers, name)
+					}
+				}
+				// now request reviews
+				reviewers := github.ReviewersRequest{Reviewers: gUsers}
+				_, resp, err := client.PullRequests.RequestReviewers(*ctx, owner, repo, int(number), reviewers)
+				if err != nil {
+					log.Println("problem adding reviewers")
+					log.Println(resp, err)
+				}
+			}
+		}
 
 	}
 }
